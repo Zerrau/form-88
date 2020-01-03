c6f58058beef81a007cb9ba03e71c1eb574cfa8c
Zerrau <mogilinsa@mail.ru>
2020-01-03 23:16:53 +0300

    Full (4,5)

--- 4 all_form_fields.py
+++ 4 all_form_fields.py
@@ -108,8 +108,8 @@ try:
     browser.find_element_by_xpath(
         '/html/body/div/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[4]/div[2]/div[12]/div/label').click()
     browser.find_element_by_xpath(
-        '/html/body/div/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[4]/div[2]/div[13]/div/label').click()"""
-    browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[2]').click()
+        '/html/body/div/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[4]/div[2]/div[13]/div/label').click()
+        browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[2]').click()
     # (2)
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[2]/div[6]/div[1]/input').send_keys(
@@ -227,11 +227,11 @@ try:
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[4]/div/div/div[2]/div[1]/div/input').send_keys(
         'A')
-    subject = browser.find_element_by_xpath(
+    subject_3 = browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[4]/div/div/div[2]/div[2]/div[1]/div/input')
-    subject.send_keys('Санкт')
-    subject.send_keys(Keys.DOWN)
-    subject.send_keys(Keys.ENTER)
+    subject_3.send_keys('Санкт')
+    subject_3.send_keys(Keys.DOWN)
+    subject_3.send_keys(Keys.ENTER)
 
     city_3 = browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[4]/div/div/div[2]/div[4]/div[1]/div/input')
@@ -255,31 +255,249 @@ try:
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[4]/div/div/div[2]/div[8]/div/input').send_keys(
         '1177746126040')
-    #  browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[4]').click()
-    """# (4)
+        browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[4]').click()
+    # (4)
     browser.find_element_by_xpath(
-        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[2]/div[1]/div/label').click()
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[2]/div[2]/div/label').click()
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[1]/div/label').click()
+    invalid = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[1]/div/div/div/div/div[1]/div[2]/div/input')
+    invalid.send_keys('Первая')
+    invalid.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[2]/div/label').click()
+    period = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[2]/div/div/div/div/div[1]/div[2]/div/input')
+    period.send_keys('Один')
+    period.send_keys(Keys.ENTER)
+
+    date_period = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[3]/div/div[1]/div/input')
+    date_period.send_keys(date_now)
+    date_period.send_keys(Keys.ENTER)
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[4]/div[2]/div/label').click()
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[5]/div/textarea').send_keys(
+        'А')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[6]/div/textarea').send_keys(
+        'А')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[7]/div/input').send_keys(
+        '50')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[8]/div/label').click()
+    term_of_disability = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[8]/div/div/div/div/div[1]/div[2]/div/input')
+    term_of_disability.send_keys('6')
+    term_of_disability.send_keys(Keys.ENTER)
+
+    date_disability = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/div[2]/div[9]/div/div/div/input')
+    date_disability.send_keys(date_now)
+    date_disability.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[1]/div/input').send_keys('А')
+    Subject_4_1 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[2]/div/div/input')
+    Subject_4_1.send_keys('Санкт')
+    Subject_4_1.send_keys(Keys.DOWN)
+    Subject_4_1.send_keys(Keys.ENTER)
+
+    citi_4_1 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[4]/div/div/input')
+    citi_4_1.send_keys('п')
+    citi_4_1.send_keys(Keys.DOWN)
+    citi_4_1.send_keys(Keys.ENTER)
+
+    street_4_1 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[5]/div/div/input')
+    street_4_1.send_keys('п')
+    street_4_1.send_keys(Keys.DOWN)
+    street_4_1.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[6]/div/input').send_keys('1')
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[7]/div/input').send_keys(
         '111111')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[1]/div/input').send_keys(
+        'Высшее образование')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[3]/div/input').send_keys(
+        'Профессия')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[5]/div/input').send_keys(
+        'Основная профессия')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[6]/div/input').send_keys(
+        'Квалификация')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[7]/div/input').send_keys('1')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[9]/div/input').send_keys(
+        'Cпециальность')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[10]/div/input').send_keys(
+        'Должность')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[11]/div/input').send_keys(
+        'Место работы')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[12]/div/input').send_keys(
+        'Условия и хар.')
+
+    Subject_4_2 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[14]/div/div[1]/div/div/input')
+    Subject_4_2.send_keys('Санкт')
+    Subject_4_2.send_keys(Keys.DOWN)
+    Subject_4_2.send_keys(Keys.ENTER)
+
+    citi_4_2 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[14]/div/div[3]/div/div/input')
+    citi_4_2.send_keys('п')
+    citi_4_2.send_keys(Keys.DOWN)
+    citi_4_2.send_keys(Keys.ENTER)
+
+    street_4_2 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[14]/div/div[4]/div/div/input')
+    street_4_2.send_keys('п')
+    street_4_2.send_keys(Keys.DOWN)
+    street_4_2.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[14]/div/div[5]/div/input').send_keys(
+        '1')
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div/div[14]/div/div[6]/div/input').send_keys(
         '111111')
+        """
     browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[5]').click()
+
     # (5)
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[1]/div[2]/div[1]/input').send_keys(
         date_year)
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[2]/div[1]/textarea').send_keys('A')
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[3]/div/textarea').send_keys('A')
+
+    oneDate_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[4]/div[2]/div/div/div[1]/div/div/div/input')
+    oneDate_5.send_keys(date_now)
+    oneDate_5.send_keys(Keys.ENTER)
+
+    endDate_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[4]/div[2]/div/div/div[2]/div/div/div/input')
+    endDate_5.send_keys(date_now)
+    endDate_5.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[4]/div[2]/div/div/div[3]/div/input').send_keys(
+        'Простуда')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[4]/div[3]/button').click()
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[5]/div/label').click()
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[6]/div[1]/input').send_keys(
+        '908765432123')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[2]/div[1]/div/input').send_keys(
+        '123')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[2]/div[2]/div/input').send_keys(
+        '123')
+
+    data_protocol_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[2]/div[3]/div/div[1]/div/input')
+    data_protocol_5.send_keys(date_now)
+    data_protocol_5.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[5]/div/textarea').send_keys(
+        'Тест Тест Тест')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[3]/div/label').click()
+    recovery_functions_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[3]/div/div/div/div/div[1]/div[2]/div/input')
+    recovery_functions_5.send_keys('полное')
+    recovery_functions_5.send_keys(Keys.ENTER)
+
+    achievement_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[1]/div[7]/div[4]/div/div/div/div/div[1]/div[2]/div/input')
+    achievement_5.send_keys('полное')
+    achievement_5.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[1]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[2]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[4]/div/input').send_keys(
+        '123')
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[7]/div[2]/div/textarea').send_keys(
         'A')
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[8]/div[2]/div/textarea').send_keys(
         'A')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[5]/div[2]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[5]/div[3]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[5]/div[4]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[5]/div[5]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[6]/div[1]/div/input').send_keys(
+        '123')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[2]/div[2]/div[6]/div[2]/div/input').send_keys(
+        '123')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[1]/div/input').send_keys(
+        'Основное')
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[2]/button').click()
     mkb = browser.find_element_by_xpath(
@@ -288,6 +506,60 @@ try:
     mkb.send_keys(Keys.ENTER)
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[2]/div[2]/div/div/div[2]/button').click()
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[3]/div/input').send_keys(
+        'Осложнения')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[4]/div/input').send_keys(
+        'Сопутствующие')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[5]/button').click()
+    mkb2 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[5]/div[2]/div/div/div[1]/div/input')
+    mkb2.send_keys('D00.1')
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[5]/div[2]/div/div/div[2]/div/ul/li[2]/ul/li[16]/ul/li[1]/ul/li[2]/a').click()
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[5]/div[2]/div/div/div[3]/button').click()
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[6]/div/input').send_keys(
+        'осложнения2')
+
+    prognosis_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[7]/div/div/div/div/div[1]/div[2]/div/input')
+    prognosis_5.send_keys('благоприятный')
+    prognosis_5.send_keys(Keys.ENTER)
+
+    potential_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[8]/div/div/div/div/div[1]/div[2]/div/input')
+    potential_5.send_keys('высокий')
+    potential_5.send_keys(Keys.ENTER)
+
+    rehabilitation_5 = browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[2]/div[9]/div/div/div/div/div[1]/div[2]/div/input')
+    rehabilitation_5.send_keys('благоприятный')
+    rehabilitation_5.send_keys(Keys.ENTER)
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[3]/div[1]/div[2]/div/textarea').send_keys(
+        'Рекоменации')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[3]/div[2]/div[2]/div/textarea').send_keys(
+        'Рекоменации')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[3]/div[3]/div[2]/div/textarea').send_keys(
+        'Рекоменации')
+
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[3]/div[3]/div[4]/div[2]/div/textarea').send_keys(
+        'Рекоменации')
+
     browser.find_element_by_xpath(
         '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div[4]/div[1]/div/label').click()
     chairman = browser.find_element_by_xpath(
@@ -301,9 +573,13 @@ try:
     member.send_keys('Т')
     time.sleep(0.5)
     member.send_keys(Keys.ENTER)
-    browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[7]').click()
-    # (7)"""
+    browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[6]').click()
 
+    # (6)
+    browser.find_element_by_xpath(
+        '/html/body/div[1]/div/div/div/form/div/div[2]/div/fieldset/div/div/div[2]/div/label').click()
+# browser.find_element_by_xpath('/html/body/div[1]/div/div/div/form/div/div[1]/ul/li[7]').click()
+# (7)"""
 
 finally:
     time.sleep(5)
