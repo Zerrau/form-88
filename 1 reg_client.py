babf9f8854f56e2459219746e040c2c268be48e4
Zerrau <mogilinsa@mail.ru>
2020-01-03 17:00:29 +0300

    Add assert

--- 1 reg_client.py
+++ 1 reg_client.py
@@ -18,7 +18,6 @@ try:
     browser.find_element_by_xpath('//*[@id="lastName"]').send_keys('Фамилия')
     browser.find_element_by_xpath('//*[@id="firstName"]').send_keys('Имя')
     browser.find_element_by_xpath('//*[@id="secondName"]').send_keys('Отчество')
-    #
     browser.find_element_by_xpath(
         '/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[1]/fieldset/div[4]/div/label').click()
     sex = browser.find_element_by_xpath(
@@ -38,10 +37,30 @@ try:
 
     # Ищем созданного пациента
     browser.find_element_by_xpath(
-        '//*[@id="root"]/div/div/div/div[2]/div/div[2]/div[1]/div[2]/div/div[1]/input').send_keys('Фамилия')
+        '//*[@id="root"]/div/div/div/div[2]/div/div[2]/div[1]/div[2]/div/div[1]/input').send_keys(
+        'Фамилия Имя Отчество')
     browser.find_element_by_xpath(
-        '//*[@id="root"]/div/div/div/div[2]/div/div[2]/div[1]/div[3]/div[1]/div/div[1]').click()
-    browser.find_element_by_xpath('/html/body/div/div/div/div/div[3]/div/div[3]/div/div/button').click()
+        '/html/body/div/div/div/div/div[2]/div/div[2]/div[1]/div[3]/div[1]/div/div[1]').click()
+    browser.find_element_by_xpath('/html/body/div/div/div/div/div[3]/div/div[2]/button').click()
+    # Проверяем на сохранение
+    lastName = browser.find_element_by_xpath(
+        '/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[1]/fieldset/div[1]/div/input').get_attribute('value')
+    assert lastName == 'Фамилия', 'LastName does not match'
+    firstName = browser.find_element_by_xpath(
+        '/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[1]/fieldset/div[2]/div/input').get_attribute('value')
+    assert firstName == 'Имя', 'FirstName does not match'
+    PatrName = browser.find_element_by_xpath(
+        '/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[1]/fieldset/div[3]/div/input').get_attribute('value')
+    assert PatrName == 'Отчество', 'PatrName does not match'
+    sex = browser.find_element_by_xpath(
+        '/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[1]/fieldset/div[4]/div/div/div/div/div[1]/div[1]')
+    assert sex.text == 'Мужчина', 'Sex does not match'
+    birthDate = browser.find_element_by_xpath(
+        '/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[1]/fieldset/div[5]/div/div/div/input').get_attribute(
+        'value')
+    assert birthDate == '25.07.1999', 'birthDate does not match'
+    browser.find_element_by_xpath('/html/body/div/div/div/div/div[4]/div/div[2]/div/form/div[2]/button').click()
+    assert patient_successfully.text == 'Данные пациента успешно сохранены', 'Patient not registered'
 
 finally:
     time.sleep(5)
